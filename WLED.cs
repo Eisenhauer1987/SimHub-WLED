607c28d2e937b5654abc63ce2b2ececde3de8abc
Eisenhauer1987 <38104423+Eisenhauer1987@users.noreply.github.com>
2026-07-09 07:08:06 +0200

    Update WLED.cs
    Blink yellow flags

--- WLED.cs
+++ WLED.cs
@@ -16,11 +16,28 @@ namespace Halcyon.WLED
         private bool yellowBlinkState = false;
         private DateTime lastYellowBlinkToggle = DateTime.MinValue;
 
+        /// <summary>
+        /// Instance of the current plugin manager
+        /// </summary>
         public PluginManager PluginManager { get; set; }
 
+        /// <summary>
+        /// Gets the left menu icon. Icon must be 24x24 and compatible with black and white display.
+        /// </summary>
         public ImageSource PictureIcon => this.ToIcon(Properties.Resources.sdkmenuicon);
+
+        /// <summary>
+        /// Gets a short plugin title to show in left menu. Return null if you want to use the title as defined in PluginName attribute.
+        /// </summary>
         public string LeftMenuTitle => "WLED";
 
+        /// <summary>
+        /// Called one time per game data update, contains all normalized game data,
+        /// raw data are intentionnally "hidden" under a generic object type (A plugin SHOULD NOT USE IT)
+        /// This method is on the critical path, it must execute as fast as possible and avoid throwing any error
+        /// </summary>
+        /// <param name="pluginManager"></param>
+        /// <param name="data">Current game data, including current and previous data frame.</param>
         public void DataUpdate(PluginManager pluginManager, ref GameData data)
         {
             LedHelper.LedAmount = Settings.ledAmount;
@@ -30,6 +47,7 @@ namespace Halcyon.WLED
 
             if (!data.GameRunning)
             {
+                LedHelper.TurnOff();
                 return;
             }
 
@@ -43,15 +61,7 @@ namespace Halcyon.WLED
                     lastYellowBlinkToggle = DateTime.Now;
                 }
 
-                if (yellowBlinkState)
-                {
-                    LedHelper.SetSolidColor(Colors.Yellow);
-                }
-                else
-                {
-                    LedHelper.SetSolidColor(Colors.Black);
-                }
-
+                LedHelper.ShowYellowBlink(yellowBlinkState);
                 return;
             }
 
@@ -62,16 +72,31 @@ namespace Halcyon.WLED
             LedHelper.ShowRpm(rpm, maxRpm, shouldShift);
         }
 
+        /// <summary>
+        /// Called at plugin manager stop, close/dispose anything needed here !
+        /// Plugins are rebuilt at game change
+        /// </summary>
+        /// <param name="pluginManager"></param>
         public void End(PluginManager pluginManager)
         {
             this.SaveCommonSettings("GeneralSettings", Settings);
         }
 
+        /// <summary>
+        /// Returns the settings control, return null if no settings control is required
+        /// </summary>
+        /// <param name="pluginManager"></param>
+        /// <returns></returns>
         public System.Windows.Controls.Control GetWPFSettingsControl(PluginManager pluginManager)
         {
             return new SettingsControl(this);
         }
 
+        /// <summary>
+        /// Called once after plugins startup
+        /// Plugins are rebuilt at game change
+        /// </summary>
+        /// <param name="pluginManager"></param>
         public void Init(PluginManager pluginManager)
         {
             SimHub.Logging.Current.Info("Starting plugin");
