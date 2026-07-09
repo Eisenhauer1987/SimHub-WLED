b31ee356bf7f38c4f006e338815536b54f504969
Eisenhauer1987 <38104423+Eisenhauer1987@users.noreply.github.com>
2026-07-09 10:18:55 +0200

    add WLEDSettings
    colors

--- WLEDSettings.cs
+++ WLEDSettings.cs
@@ -1,17 +1,15 @@
-﻿namespace Halcyon.WLED
+namespace Halcyon.WLED
 {
-    /// <summary>
-    /// Settings class, make sure it can be correctly serialized using JSON.net
-    /// </summary>
     public class WLEDSettings
     {
-        public string stripUrl = "wled-table.local";
-        public int stripPort = 21324;
+        public string stripUrl { get; set; } = "wled-table.local";
+        public int stripPort { get; set; } = 21324;
+        public int ledAmount { get; set; } = 60;
+        public int offset { get; set; } = 0;
+        public bool mirror { get; set; } = false;
+        public bool center { get; set; } = false;
 
-        public int ledAmount = 60;
-        public int offset = 0;
-
-        public bool mirror = false;
-        public bool center = false;
+        public string RiseColor { get; set; } = "#0000FF";
+        public string MaxColor { get; set; } = "#FF0000";
     }
 }
